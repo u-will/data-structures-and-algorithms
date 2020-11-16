162e984d598ab70e50857ea21c1c276d8b7e973f
Ulrich Wilfried Motchoffo <54010264+u-will@users.noreply.github.com>
2020-11-16 10:04:19 -0800

    Update test_linkedList.py

--- python/code_challenges/linked-list/tests/test_linkedList.py
+++ python/code_challenges/linked-list/tests/test_linkedList.py
@@ -4,37 +4,19 @@ def test_instantiation():
 	initialList = LinkedList()
 	assert initialList.head == None
 
+def test_multiple_ins():
+	initialList = LinkedList()
+	initialList.insert(9)
+	initialList.insert(2)
+	assert initialList.head.value == 2 and initialList.head.next
+    
+    
 def test_insertion():
 	initialList = LinkedList()
-	initialList.insert(5)
-	assert initialList.head.value == 5
-def test_insertion_again():
-	initialList = LinkedList()
-	initialList.insert(5)
-	initialList.insert(6)
-	assert initialList.head.value == 6
-
-def test_multiple_inserts():
-	initialList = LinkedList()
-	initialList.insert(5)
-	initialList.insert(6)
-	assert initialList.head.value == 6 and initialList.head.next
-
-def test_truthy_includes():
-	initialList = LinkedList()
-	initialList.insert(5)
-	initialList.insert(6)
 	initialList.insert(7)
-	initialList.insert(8)
-	assert initialList.includes(6)
+	assert initialList.head.value == 7
 
-def test_falsy_includes():
-	initialList = LinkedList()
-	initialList.insert(5)
-	initialList.insert(6)
-	initialList.insert(7)
-	initialList.insert(8)
-	assert initialList.includes(9) == False
+
 
 def test_return_all_values():
 	initialList = LinkedList()
